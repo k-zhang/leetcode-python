e4d8419e73b7efd6aac4288ea4c3ee8408fd7501
Kefeng Zhang <kzhangtech@gmail.com>
2021-05-28 16:49:20 +0100

    Add find median sorted arrays

---
+++ answers/tests/test_median_of_two_sorted_arrays.py
@@ -0,0 +1,20 @@
+import unittest
+from median_of_two_sorted_arrays import *
+
+
+class MyTestCase(unittest.TestCase):
+    def test(self):
+        solution = Solution()
+        self.assertEqual(2.0, solution.find_median_sorted_arrays([1, 3], [2]))
+        self.assertEqual(2.0, solution.find_median_sorted_arrays([1, 2, 3], []))
+        self.assertEqual(2.5, solution.find_median_sorted_arrays([1, 2, 3, 4], []))
+        self.assertEqual(2.5, solution.find_median_sorted_arrays([], [1, 2, 3, 4]))
+        self.assertEqual(2.5, solution.find_median_sorted_arrays([1, 2], [3, 4]))
+        self.assertEqual(0.0, solution.find_median_sorted_arrays([0, 0], [0, 0]))
+        self.assertEqual(1.0, solution.find_median_sorted_arrays([], [1]))
+        self.assertEqual(2.0, solution.find_median_sorted_arrays([2], []))
+        self.assertEqual(0.0, solution.find_median_sorted_arrays([], []))
+
+
+if __name__ == '__main__':
+    unittest.main()
